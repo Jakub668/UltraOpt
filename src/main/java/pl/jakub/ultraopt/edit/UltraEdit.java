a0989c406054d4c8a48cd5a652eb16acb35876bc
Jakub668 <jakub.hunek19@gmail.com>
2026-01-01 00:47:14 +0100

    Create UltraEdit.java

---
+++ src/main/java/pl/jakub/ultraopt/edit/UltraEdit.java
@@ -0,0 +1,7 @@
+package pl.jakub.ultraopt.edit;
+
+public class UltraEdit {
+
+    public static final Selection SELECTION = new Selection();
+    public static final Clipboard CLIPBOARD = new Clipboard();
+}
