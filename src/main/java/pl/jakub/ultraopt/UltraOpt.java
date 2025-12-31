a04543e8b6887c4aa3beb32b01778950f6c08ff4
Jakub668 <jakub.hunek19@gmail.com>
2025-12-31 23:49:59 +0100

    Update UltraOpt.java

--- src/main/java/pl/jakub/ultraopt/UltraOpt.java
+++ src/main/java/pl/jakub/ultraopt/UltraOpt.java
@@ -1,1 +1,28 @@
+package pl.jakub.ultraopt;
 
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import pl.jakub.ultraopt.mods.ModListCommand;
+import pl.jakub.ultraopt.settings.UltraOptSettingsCommand;
+import pl.jakub.ultraopt.edit.commands.Pos1Command;
+import pl.jakub.ultraopt.edit.commands.Pos2Command;
+
+public class UltraOpt implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+
+            // /ultraopt (ON / OFF)
+            UltraOptSettingsCommand.register(dispatcher);
+
+            // /modlist
+            ModListCommand.register(dispatcher);
+
+            // UltraEdit basic
+            Pos1Command.register(dispatcher);
+            Pos2Command.register(dispatcher);
+        });
+    }
+}
