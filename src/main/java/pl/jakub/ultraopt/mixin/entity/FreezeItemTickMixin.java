5930bc12e112137b3aad9ebe6c49d151294cd5de
Jakub668 <jakub.hunek19@gmail.com>
2026-01-01 14:00:02 +0100

    Create FreezeItemTickMixin.java

---
+++ src/main/java/pl/jakub/ultraopt/mixin/entity/FreezeItemTickMixin.java
@@ -0,0 +1,16 @@
+package pl.jakub.ultraopt.mixin.entity;
+
+import net.minecraft.entity.ItemEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemEntity.class)
+public class FreezeItemTickMixin {
+
+    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+    private void ultraopt$skipItemTick(CallbackInfo ci) {
+        ci.cancel();
+    }
+}
