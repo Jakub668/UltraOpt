8fc654b17ae61020c22a5f6887eec022853aa219
Jakub668 <jakub.hunek19@gmail.com>
2026-01-01 16:01:05 +0100

    Update UltraOptConfig.java

--- src/main/java/pl/jakub/ultraopt/config/UltraOptConfig.java
+++ src/main/java/pl/jakub/ultraopt/config/UltraOptConfig.java
@@ -1,15 +1,8 @@
-// UltraOptConfig.java
-// Wszystkie ON / OFF
-
 package pl.jakub.ultraopt.config;
 
 public class UltraOptConfig {
-
-    // GLOBAL
     public boolean enabled = true;
-
-    // ITEMY
-    public boolean itemPhysics = true;   // przyklejenie do ziemi
-    public boolean itemSingleModel = true; // jeden model
-    public boolean itemCounter = true;   // licznik nad itemem
+    public boolean itemPhysics = true;
+    public boolean itemCounter = true;
+    public boolean itemSingleModel = true;
 }
