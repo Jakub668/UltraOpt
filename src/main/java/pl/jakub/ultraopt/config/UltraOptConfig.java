86cfe477c063e06c707568a9062caae838a214bc
Jakub668 <jakub.hunek19@gmail.com>
2026-01-03 15:46:47 +0100

    Update UltraOptConfig.java

--- src/main/java/pl/jakub/ultraopt/config/UltraOptConfig.java
+++ src/main/java/pl/jakub/ultraopt/config/UltraOptConfig.java
@@ -3,8 +3,9 @@ package pl.jakub.ultraopt.config;
 public class UltraOptConfig {
 
     public boolean enabled = true;
-    public boolean itemPhysics = true;
+
     public boolean itemCounter = true;
+    public boolean itemPhysics = true;
     public boolean itemSingleModel = true;
 
 }
