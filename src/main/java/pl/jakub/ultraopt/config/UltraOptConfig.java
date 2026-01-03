4cc2a6dac982c191a17e16a25dec6095846cb327
Jakub668 <jakub.hunek19@gmail.com>
2026-01-03 15:34:39 +0100

    Update UltraOptConfig.java

--- src/main/java/pl/jakub/ultraopt/config/UltraOptConfig.java
+++ src/main/java/pl/jakub/ultraopt/config/UltraOptConfig.java
@@ -2,14 +2,9 @@ package pl.jakub.ultraopt.config;
 
 public class UltraOptConfig {
 
-    // główny przełącznik moda
     public boolean enabled = true;
-
-    // itemy
     public boolean itemPhysics = true;
     public boolean itemCounter = true;
     public boolean itemSingleModel = true;
 
-    // particle (na przyszłość)
-    public boolean particles = true;
 }
