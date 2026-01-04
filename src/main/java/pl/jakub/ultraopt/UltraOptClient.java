ecdd5b51ecaf9a066f18a457c8abac46f4c70565
Jakub668 <jakub.hunek19@gmail.com>
2026-01-04 11:25:08 +0100

    Update UltraOptClient.java

--- src/main/java/pl/jakub/ultraopt/UltraOptClient.java
+++ src/main/java/pl/jakub/ultraopt/UltraOptClient.java
@@ -1,13 +1,12 @@
 package pl.jakub.ultraopt;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import pl.jakub.ultraopt.client.overlay.ItemCountOverlay;
+import pl.jakub.ultraopt.client.render.ItemCountWorldRenderer;
 
 public class UltraOptClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        HudRenderCallback.EVENT.register(new ItemCountOverlay());
+        ItemCountWorldRenderer.register();
     }
 }
